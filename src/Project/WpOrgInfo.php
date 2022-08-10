d153d5324a13ae7d87aa712590911de73ee77dd6
Miguel Muscat <miguelmuscat93@gmail.com>
2022-08-10 12:36:10 +0200

    Add WpOrgInfo

---
+++ src/Project/WpOrgInfo.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace RebelCode\Mantle\Project;
+
+use InvalidArgumentException;
+
+class WpOrgInfo
+{
+    /** @var string|null */
+    public $slug = null;
+    /** @var string|null */
+    public $name = null;
+    /** @var string[] */
+    public $tags = [];
+    /** @var string[] */
+    public $contributors = [];
+    /** @var string|null */
+    public $testedUpTo = null;
+
+    /**
+     * Constructor.
+     *
+     * @param string|null $slug The slug, or URL ID, of the plugin on WordPress.org.
+     * @param string|null $name The human-friendly name of the plugin.
+     * @param string|null $testedUpTo The latest version of WordPress that this plugin has been tested with.
+     * @param string[] $tags The list of tags for the plugin.
+     * @param string[] $contributors The list of contributors for the plugin.
+     */
+    public function __construct(
+        ?string $slug = null,
+        ?string $name = null,
+        ?string $testedUpTo = null,
+        array $tags = [],
+        array $contributors = []
+    ) {
+        $this->slug = $slug;
+        $this->name = $name;
+        $this->testedUpTo = $testedUpTo;
+        $this->tags = $tags;
+        $this->contributors = $contributors;
+    }
+
+    /** Creates an instance from an array. */
+    public static function fromArray(array $data): self
+    {
+        $instance = new self();
+
+        foreach ($data as $key => $value) {
+            if (!property_exists($instance, $key)) {
+                throw new InvalidArgumentException("Invalid property \"$key\" in wordpress.org meta data");
+            }
+
+            $instance->$key = $value;
+        }
+
+        return $instance;
+    }
+}
---
+++ tests/Mantle/Project/WpOrgInfoTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace RebelCode\Mantle\Tests\Project;
+
+use InvalidArgumentException;
+use RebelCode\Mantle\Project\WpOrgInfo;
+use PHPUnit\Framework\TestCase;
+
+class WpOrgInfoTest extends TestCase
+{
+    public function test_it_should_construct()
+    {
+        $slug = 'my-test-plugin';
+        $name = 'My Test Plugin';
+        $testedUpTo = '4.0';
+        $tags = ['tag1', 'tag2'];
+        $contributors = ['contributor1', 'contributor2'];
+
+        $info = new WpOrgInfo($slug, $name, $testedUpTo, $tags, $contributors);
+
+        $this->assertSame($slug, $info->slug);
+        $this->assertSame($name, $info->name);
+        $this->assertSame($testedUpTo, $info->testedUpTo);
+        $this->assertSame($tags, $info->tags);
+        $this->assertSame($contributors, $info->contributors);
+    }
+
+    public function test_it_should_construct_from_array()
+    {
+        $array = [
+            'slug' => 'test-plugin',
+            'name' => 'Test Plugin',
+            'testedUpTo' => '4.0',
+            'tags' => ['tag1', 'tag2'],
+            'contributors' => ['contributor1', 'contributor2'],
+        ];
+
+        $info = WpOrgInfo::fromArray($array);
+
+        $this->assertSame($array['slug'], $info->slug);
+        $this->assertSame($array['name'], $info->name);
+        $this->assertSame($array['testedUpTo'], $info->testedUpTo);
+        $this->assertSame($array['tags'], $info->tags);
+        $this->assertSame($array['contributors'], $info->contributors);
+    }
+
+    public function test_it_should_construct_from_array_with_defaults()
+    {
+        $info = WpOrgInfo::fromArray([]);
+
+        $this->assertNull($info->slug);
+        $this->assertNull($info->name);
+        $this->assertNull($info->testedUpTo);
+        $this->assertEquals([], $info->tags);
+        $this->assertEquals([], $info->contributors);
+    }
+
+    public function test_it_should_throw_exception_with_unknown_array_key()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessageMatches('/"unknown"/');
+
+        $array = [
+            'slug' => 'test-plugin',
+            'name' => 'Test Plugin',
+            'testedUpTo' => '4.0',
+            'tags' => ['tag1', 'tag2'],
+            'contributors' => ['contributor1', 'contributor2'],
+            'unknown' => 'value',
+        ];
+
+        WpOrgInfo::fromArray($array);
+    }
+}
